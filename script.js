3c059f4a86db21993f4303c61dd0a72a15acc7e0
josewdr <josewdr@gmail.com>
2021-10-17 19:02:44 -0600

    Actualización del proyecto final del curso de de ibm fullstak

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
+//Creamos una funcion que enlace la etiqueta <span> con el imput type "range", de nuestro html
+function updateRate(){
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval; //El atributo .innetText, permite reescribir el valor actual del "rate", en la etiqueta <span>
+}
+
+
+//La siguiente funcion, hace los calculos que permite que las personas sepan cuanto recibirán si depositan "x" cantidad de dinero y lo mantienen en el banco durante "x" años por "x" tasa de interés.
 function compute()
 {
-    p = document.getElementById("principal").value;
+    //Hacemos llamado a cada uno de los "input" de nuestro html
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var year = document.getElementById("years").value;
     
+    //Hacemos la operacion que nos permite saber cuál será el monto que recibirá una persona que deposita su dinero en el banco.
+    var amount = principal * year * rate/100
+     
+    //Validamos que el número ingresado no sea ni negativo ni cero 
+    if(principal <= 0){
+        alert( "Enter a positive number")
+        principal.focus(); //En caso de que esta esta parte de la función se ejecu, se seleccionar´
+        return false;
+    }else{
+        //En caso se que el número sea entero, se escribe la stiqueta <span> id = "result" el siguiente información.
+        document.getElementById("result").innerHTML = "If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+    }
 }
         
